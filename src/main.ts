f98ff5e78d9feabac54ab9c1e9c8d60c6918f111
muizu <muizu0920555@protonmail.com>
2023-09-15 20:18:55 +0900

    first commit

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,11 @@
-import express from 'express';
+import express, { Request, Response, NextFunction} from 'express';
+
 
 const app = express();
+
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    res.status(500).json({message: err.message});
+});
 
 app.listen(8000, '0.0.0.0');
