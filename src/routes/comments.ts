209aa7d001075060e3185bfa4abb1d1881742c26
muizu <muizu0920555@protonmail.com>
2023-09-25 19:59:17 +0900

    imple-plusComment

--- src/routes/comments.ts
+++ src/routes/comments.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { postComment , getComment } from "../controllers/comments"
+import { postComment , getComment, plusComment } from "../controllers/comments"
 import { authenticateUser } from "../middlewares/auth";
 
 const router = Router();
@@ -7,5 +7,7 @@ const router = Router();
 router.get("/", getComment);
 
 router.post("/board/:id", authenticateUser , postComment);
+
+router.put("/puls", authenticateUser, plusComment);
 
 export default router;
